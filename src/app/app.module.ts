183790f40449a4db89bda58ffbb57229f7383d47
jbcharrier <jbcharrier33@gmail.com>
2017-09-10 10:05:25 +0200

    application ok before authentication and date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import { TodoDataService } from './todo-data.service';
+import { TodoListHeaderComponent } from './todo-list-header/todo-list-header.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
+import { TodoListFooterComponent } from './todo-list-footer/todo-list-footer.component';
+import { ApiService } from './api.service';
+import { AppRoutingModule } from './app-routing.module';
+import { TodosComponent } from './todos/todos.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListHeaderComponent,
+    TodoListComponent,
+    TodoListItemComponent,
+    TodoListFooterComponent,
+    TodosComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [TodoDataService, ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/todo-data.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Todo } from './todo';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class TodoDataService {
+
+  lastId: number = 0;
+  todos: Todo[] = [];
+
+  constructor(private api: ApiService) { }
+
+  addTodo(todo: Todo): Observable<Todo> {
+    return this.api.createTodo(todo);
+  }
+
+  deleteTodoById(todoId: number): Observable<Todo> {
+    return this.api.deleteTodoById(todoId);
+  }
+
+  updateTodoById(todo: Todo): Observable<Todo> {
+    return this.api.updateTodo(todo);
+  }
+
+  getAllTodos(): Observable<Todo[]>{
+    return this.api.getAllTodos();
+  }
+
+  getTodoById(todoId: number): Observable<Todo> {
+    return this.api.getTodoById(todoId);
+  }
+
+  toggleTodoComplete(todo: Todo) {
+    todo.complete = !todo.complete;
+    return this.api.updateTodo(todo);
+  }
+
+}
